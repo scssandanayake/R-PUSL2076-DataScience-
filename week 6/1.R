ab98f7006abc4344c0f3eff17d5366bf1172e1b5
scssandanayake <114943338+scssandanayake@users.noreply.github.com>
2023-11-23 10:10:28 +0530

    week 6 added

---
+++ week 6/1.R
@@ -0,0 +1,39 @@
+#packages
+install.packages("ISLR") #orange juice dataset
+install.packages("caret") #workflow
+install.packages("rpart.plot") #draw the disision tree
+install.packages("tidyverse")
+install.packages("skimr") #summary skewness
+
+library(ISLR)
+library(caret)
+library(rpart.plot)
+library(tidyverse)
+library(skimr)
+
+#setting the dataset
+
+dataset <- OJ
+dataset
+skim_to_wide(dataset)
+
+#training data & testing data
+splitt <- createDataPartition(y = dataset$Purchase, p = 0.75, list = FALSE)
+train <- dataset[splitt, ]
+test <- dataset[splitt, ]
+
+dim(train)
+dim(test)
+
+#the classifier
+set.seed(15000)
+
+dec_tree <- rpart(formula = Purchase ~. ,
+                  data = train,
+                  method = "class",
+                  xval = 10 )
+
+#drawing the tree
+rpart.plot(dec_tree, yesno = TRUE)
+
+
---
+++ week 6/2.R
@@ -0,0 +1,9 @@
+#creating data frame
+df <- data.frame(
+  col1=c(100,200,300,300,NA),
+  col2=c(100,200,300,300,NA),
+  col3=c(100,200,300,300,NA)
+)
+df
+
+df$col1
